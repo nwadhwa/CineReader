35bb50f44c519f3675a582f2fa05a9401a81d429
Neal Wadhwa <nealian@gmail.com>
2014-03-15 21:03:05 -0400

    Added test for primitive data type sizes

--- test.cpp
+++ test.cpp
@@ -1,12 +1,42 @@
 #include "data.h"
 #include <iostream>
+#include <assert.h>
 
 using namespace std;
 
 int main() {
   // Verify sizes of all datatypes in data.h
-  DWORD n;
-  cout << sizeof(n) << endl;
+  BYTE m_byte;
+  CHAR m_char;
+  WORD m_word;
+  INT16 m_int16;
+  SHORT m_short;
+  BOOL m_bool;
+  DWORD m_dword;
+  UINT m_uint;
+  LONG m_long;
+  INT m_int;
+  FLOAT m_float;
+  DOUBLE m_double;
+
+  assert(sizeof(m_byte) == 1);
+  assert(sizeof(m_char) == 1);
+
+  assert(sizeof(m_word) == 2);
+  assert(sizeof(m_int16) == 2);
+  assert(sizeof(m_short) == 2);
+  
+  assert(sizeof(m_bool) == 4);
+  assert(sizeof(m_dword) == 4);
+  assert(sizeof(m_uint) == 4);
+  assert(sizeof(m_int) == 4);
+  assert(sizeof(m_long) == 4);
+  assert(sizeof(m_float) == 4);
+
+  assert(sizeof(m_double) == 8);
+
+  cout << "All primitive data types are the correct size." << endl;
+  
 
 
 }
