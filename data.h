0d9ab404831c694eae5fc33d9ee37247d1a07e09
Neal Wadhwa <nealian@gmail.com>
2014-03-15 21:10:15 -0400

    Added primitive structures

--- data.h
+++ data.h
@@ -17,6 +17,31 @@ typedef float FLOAT;
 typedef double DOUBLE;
 typedef std::string STRING;
 
+class TIME64 {
+ public:
+  DWORD fractions; // Fractions of second, divide by 2^32 to convert to seconds
+  DWORD seconds; // Seconds since Jan 1, 1970
+
+  TIME64();
+};
+
+class IMFILTER { //structure to define a convolution filter
+ public:
+  INT dim; //square kernel dimension
+  INT shifts; //not sure about this one
+  INT bias; // or this one
+  INT coef[25]; //coefficinets of kernel
+
+  IMFILTER();
+};
+
+class WBGAIN {
+  FLOAT R; //gain correction for red
+  FLOAT B; //gain correction for blue
+};
+
+
+
 
 
 #endif
