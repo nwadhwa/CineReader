425d8e434f4b2f5108f2a93f8f5f4980dee34987
Neal Wadhwa <nealian@gmail.com>
2014-08-30 16:30:49 -0400

    FIxed bugs and dealt with ambiguities in the file format document, seems to be working now

--- setup.cpp
+++ setup.cpp
@@ -24,7 +24,7 @@ SETUP::SETUP(std::ifstream *inputCine, const CINEFILEHEADER &cineheader) {
   BinChannels = br.readSHORT();
   SamplesPerImage = br.readBYTE();
   for(int k = 0; k < 8; k++) {
-    BinName[k] = br.readZeroTerminatedSTRING();
+    BinName[k] = br.readLengthNSTRING(11);
   }
   AnaOption = br.readWORD();
   AnaChannels = br.readSHORT();
@@ -37,17 +37,18 @@ SETUP::SETUP(std::ifstream *inputCine, const CINEFILEHEADER &cineheader) {
     AnaGain[k] = br.readFLOAT();
   }
   for(int k = 0; k < 8; k++) {
-    AnaUnit[k] = br.readZeroTerminatedSTRING();
+    AnaUnit[k] = br.readLengthNSTRING(6);
   }
+
   for (int k = 0; k< 8; k++) {
-    AnaName[k] = br.readZeroTerminatedSTRING();
-  }
+    AnaName[k] = br.readLengthNSTRING(11);
+  }  
   IFirstImage = br.readLONG();
   dwImageCount = br.readDWORD();
   nQFactor = br.readSHORT();
   wCineFileType = br.readWORD();
   for (int k = 0; k < 4; k++) {
-    szCinePath[k] = br.readZeroTerminatedSTRING();
+    szCinePath[k] = br.readLengthNSTRING(65);
   }
   bMainsFreq = br.readWORD();
   bTimeCode = br.readBYTE();
@@ -87,7 +88,7 @@ SETUP::SETUP(std::ifstream *inputCine, const CINEFILEHEADER &cineheader) {
   AutoExpLevel = br.readUINT();
   AutoExpSpeed = br.readUINT();
   for (int k = 0; k < 4; k++) 
-    AutoExpRect[k] = br.readWORD();
+    AutoExpRect[k] = br.readDWORD();
   for (int k = 0; k < 4; k++) 
     WBGain[k] = br.readWBGAIN();
   Rotate = br.readINT();
@@ -116,6 +117,7 @@ SETUP::SETUP(std::ifstream *inputCine, const CINEFILEHEADER &cineheader) {
   CICalib = br.readUINT();
   CalibWidth = br.readUINT();
   CalibHeight = br.readUINT();
+  CalibRate = br.readUINT();
   CalibExp = br.readUINT();
   CalibEDR = br.readUINT();
   CalibTemp = br.readUINT();
@@ -138,86 +140,85 @@ SETUP::SETUP(std::ifstream *inputCine, const CINEFILEHEADER &cineheader) {
 
 // Prints out all non obsolete values
 std::ostream& operator<<(std::ostream& os, const SETUP & setupheader) {
-  os << "Setup Length: " << Length << std::endl;
-  os << "Global Signal Options: " << SigOption << std::endl;
-  os << "Number of Binary Channels read from SAM: " << BinChannels << std::endl;
-  os << "Samples per Image: " << SamplesPerImage << std::endl;
+  os << "Setup Length: " << setupheader.getLength() << std::endl;
+  os << "Global Signal Options: " << setupheader.getSigOption() << std::endl;
+  os << "Number of Binary Channels read from SAM: " << setupheader.getBinChannels() << std::endl;
+  os << "Samples per Image: " << static_cast<unsigned>(setupheader.getSamplesPerImage()) << std::endl;
   os << " Binary Signal Names: " << std::endl;
-  for (int k = 0; k < 64; k++) 
-    os << "\t" << BinName[k] << std::endl;
-  os << "Global Analog Option: " << AnaOption << std::endl;
-  os << "Number of Analog Channels: " << AnaChannels << std::endl; 
-  os << "Analog Board Type: " << AnaBoard << std::endl;
+  for (int k = 0; k < 8; k++) 
+    os << "\t" << setupheader.getBinName(k) << std::endl;
+  os << "Global Analog Option: " << setupheader.getAnaOption() << std::endl;
+  os << "Number of Analog Channels: " << setupheader.getAnaChannels() << std::endl; 
+  os << "Analog Board Type: " << setupheader.getAnaBoard() << std::endl;
   os << "Per Channel Analog Options: " << std::endl;
-  for (int k = 0; k < 64; k++) 
-    os << "\t" << ChOptions[k] << std::endl;
+  for (int k = 0; k < 8; k++) 
+    os << "\t" << setupheader.getChOption(k) << std::endl;
   os << "Per Channel Gain: " <<  std::endl;
-  for (int k = 0; k < 64; k++) 
-    os << "\t" << AnaGain[k] << std::endl;
+  for (int k = 0; k < 8; k++) 
+    os << "\t" << setupheader.getAnaGain(k) << std::endl;
   os << "Per Channel Measurement Unit: " <<  std::endl;
-  for (int k = 0; k < 64; k++)     
-    os << "\t" << AnaUnit[k] << std::endl;
+  for (int k = 0; k < 8; k++)     
+    os << "\t" << setupheader.getAnaUnit(k) << std::endl;
   os << "Channel Name: " <<  std::endl;
-  for (int k = 0; k < 64; k++)     
-    os << "\t" << AnaName[k] << std::endl;
-  os << "First Image in Range: " << IFirstImage << std::endl;
-  os << "Image Count in Range: " << dwImageCount << std::endl;
-  os << "Quality (Compressed Files): " << nQFactor << std::endl;
-  os << "Cine File Type: " << wCineFileType << std::endl;
+  for (int k = 0; k < 8; k++)     
+    os << "\t" << setupheader.getAnaName(k) << std::endl;
+  os << "First Image in Range: " << setupheader.getIFirstImage() << std::endl;
+  os << "Image Count in Range: " << setupheader.getdwImageCount() << std::endl;
+  os << "Quality (Compressed Files): " << setupheader.getnQFactor() << std::endl;
+  os << "Cine File Type: " << setupheader.getwCineFileType() << std::endl;
   os << "Cine File Save Paths: " << std::endl;
   for (int k = 0; k < 4; k++) 
-    os << "\t" << szCinePath[k] << std::endl;
-  os << "Image Width: " << ImWidth << std::endl;
-  os << "Image Height: " << ImHeight << std::endl;
-  os << "Camera Serial: " << Serial << std::endl;
-  os << "Saturation: " << Saturation << std::endl;
-  os << "Auto Exposure: " << AutoExposure << std::endl;
-  os << "Flipped Horizontally: " << bFlipH << std::endl;
-  os << "Flipped Vertically: " << bFlipV << std::endl;
-  os << "Grid: " << Grid << std::endl;
-  os << "Frame Rate: " << FrameRate << std::endl;
-  os << "Number of Post Trigger Frames: " << PostTrigger << std::endl;
-  os << "Color Enabled: " << bEnableColor << std::endl;
-  os << "Camera Version: " << CameraVersion << std::endl;
-  os << "Firmware Version: " << FirmwareVersion << std::endl;
-  os << "Software Version: " << SoftwareVersion << std::endl;
-  os << "Recording Time Zone: " <<RecordingTimeZone << std::endl;
-  os << "Color Filter Array Type: " << CFA << std::endl;
-  os << "Brightness: " << Bright << std::endl;
-  os << "Contrast: " << Contrast << std::endl;
-  os << "Gamma: " << Gamma << std::endl;
-  os << "Auto Exposure Level: " << AutoExpLevel << std::endl;
-  os << "Auto Exposure Speed: " << AutoExpSpeed << std::endl;
+    os << "\t" << setupheader.getszCinePath(k) << std::endl;
+  os << "Image Width: " << setupheader.getImWidth() << std::endl;
+  os << "Image Height: " << setupheader.getImHeight() << std::endl;
+  os << "Camera Serial: " << setupheader.getSerial() << std::endl;
+  os << "Saturation: " << setupheader.getSaturation() << std::endl;
+  os << "Auto Exposure: " << setupheader.getAutoExposure() << std::endl;
+  os << "Flipped Horizontally: " << setupheader.getbFlipH() << std::endl;
+  os << "Flipped Vertically: " << setupheader.getbFlipV() << std::endl;
+  os << "Grid: " << setupheader.getGrid() << std::endl;
+  os << "Frame Rate: " << setupheader.getFrameRate() << std::endl;
+  os << "Number of Post Trigger Frames: " << setupheader.getPostTrigger() << std::endl;
+  os << "Color Enabled: " << setupheader.getbEnableColor() << std::endl;
+  os << "Camera Version: " << setupheader.getCameraVersion() << std::endl;
+  os << "Firmware Version: " << setupheader.getFirmwareVersion() << std::endl;
+  os << "Software Version: " << setupheader.getSoftwareVersion() << std::endl;
+  os << "Recording Time Zone: " << setupheader.getRecordingTimeZone() << std::endl;
+  os << "Color Filter Array Type: " << setupheader.getCFA() << std::endl;
+  os << "Brightness: " << setupheader.getBright() << std::endl;
+  os << "Contrast: " << setupheader.getContrast() << std::endl;
+  os << "Gamma: " << setupheader.getGamma() << std::endl;
+  os << "Auto Exposure Level: " << setupheader.getAutoExpLevel() << std::endl;
+  os << "Auto Exposure Speed: " << setupheader.getAutoExpSpeed() << std::endl;
   os << "White Balance Gain: " << std::endl;
   for (int k = 0; k < 4; k++) 
-    os << "\t(" << WBGain[k].R << "," << WBGain[k].B << ")\n";
-  os << "Rotate: " << Rotate << std::endl;
-  os << "Bits Per Pixel: " << RealBPP << std::endl;
-  os << "Minimum Value When Converting to 8 Bits: " << Conv8Min << std::endl;
-  os << "Maximum Value When Converting to 8 Bits: " << Conv8Max << std::endl;
-  os << "Filter Code: " << FilterCode << std::endl;
-  os << "Filter Param: " <<FilterParam << std::endl;
-  os << "Software Version for Black Reference: " << BlackCalSVer << std::endl;
-  os << "Software Version for White Reference: " << WhiteCalSVer << std::endl;
-  os << "Software Version for Gray Reference: " << GrayCalSVer << std::endl;
-  os << "Stamp Time: " << bStampTime << std::endl;
-  os << "Sound Destination: " << SoundDest << std::endl;
+    os << "\t(" << setupheader.getWBGain(k).R << "," << setupheader.getWBGain(k).B << ")\n";
+  os << "Rotate: " << setupheader.getRotate() << std::endl;
+  os << "Bits Per Pixel: " << setupheader.getRealBPP() << std::endl;
+  os << "Minimum Value When Converting to 8 Bits: " << setupheader.getConv8Min() << std::endl;
+  os << "Maximum Value When Converting to 8 Bits: " << setupheader.getConv8Max() << std::endl;
+  os << "Filter Code: " << setupheader.getFilterCode() << std::endl;
+  os << "Filter Param: " << setupheader.getFilterParam() << std::endl;
+  os << "Software Version for Black Reference: " << setupheader.getBlackCalSVer() << std::endl;
+  os << "Software Version for White Reference: " << setupheader.getWhiteCalSVer() << std::endl;
+  os << "Software Version for Gray Reference: " << setupheader.getGrayCalSVer() << std::endl;
+  os << "Stamp Time: " << setupheader.getbStampTime() << std::endl;
+  os << "Sound Destination: " << setupheader.getSoundDest() << std::endl;
   // Skipped stuff about changing frames (FRPSteps)
-  os << "Calibration Width: " << CalibWidth << std::endl;
-  os << "Calibration Height: " << CalibHeight << std::endl;
-  os << "Calibration Exposure: " << CalibExposure << std::endl;
-  os << "Calibration EDR: " << CalibEDR << std::endl;
-  os << "Calibration Temperature: " << CalibTemp << std::endl;
-  os << "Range Code: " << RangeCode << std::endl;
-  os << "Range Size: " << RangeSize << std::endl;
-  os << "Decimation: " << Decimation << std::endl;
-  os << "Master Serial: " << MasterSerial << std::endl;
-  os << "Sensor: " << Sensor << std::endl;
-  os << "Shutter (ns): " << ShutterNs << std::endl;
-  os << "EDR Shutter (ns): " << EDRShutterNs << std::endl;
-  os << "Frame Delay (ns): " << FrameDelayNs << std::endl;
-  
-  os << "Description: " << Description << std::endl;
+  os << "Calibration Width: " << setupheader.getCalibWidth() << std::endl;
+  os << "Calibration Height: " << setupheader.getCalibHeight() << std::endl;
+  os << "Calibration Exposure: " << setupheader.getCalibExp() << std::endl;
+  os << "Calibration EDR: " << setupheader.getCalibEDR() << std::endl;
+  os << "Calibration Temperature: " << setupheader.getCalibTemp() << std::endl;
+  os << "Range Code: " << setupheader.getRangeCode() << std::endl;
+  os << "Range Size: " << setupheader.getRangeSize() << std::endl;
+  os << "Decimation: " << setupheader.getDecimation() << std::endl;
+  os << "Master Serial: " << setupheader.getMasterSerial() << std::endl;
+  os << "Sensor: " << setupheader.getSensor() << std::endl;
+  os << "Shutter (ns): " << setupheader.getShutterNs() << std::endl;
+  os << "EDR Shutter (ns): " << setupheader.getEDRShutterNs() << std::endl;
+  os << "Frame Delay (ns): " << setupheader.getFrameDelayNs() << std::endl;
+  os << "Description: " << setupheader.getDescription() << std::endl;
 
 
   
--- setup.h
+++ setup.h
@@ -11,7 +11,110 @@
 class SETUP {
  public:
   SETUP(std::ifstream *inputCine, const CINEFILEHEADER &cineheader);
-  
+  /*
+  WORD FrameRate16; //obsolete
+  WORD Shutter16; //obsolete
+  WORD PostTrigger16; //obsolete
+  WORD FrameDelay16; //obsolete
+  WORD AspectRatio; //obsolete
+  WORD Contrast16; //obsolete
+  WORD Bright16; //obsolete 
+  BYTE Rotate16;//obsolete 
+  BYTE TimeAnnotation; //obsolete
+  BYTE TrigCine; //obsolete
+  BYTE TrigFrame;
+  BYTE ShutterOn; //obsolete
+  CHAR * DescriptionOLD; //obsolete
+  */
+
+  // Need to do bound checking on some of these
+  WORD getLength() const { return Length; }
+  WORD getBinning() const { return Binning; }
+  WORD getSigOption() const { return SigOption; }
+  SHORT getBinChannels() const { return BinChannels; }
+  BYTE getSamplesPerImage() const { return SamplesPerImage; }
+  STRING getBinName(int k) const { return BinName[k]; }
+  WORD  getAnaOption() const { return AnaOption; }
+  SHORT  getAnaChannels() const { return AnaChannels; }
+  BYTE getAnaBoard() const { return AnaBoard; }
+  SHORT getChOption(int k) const { return ChOption[k]; }
+  FLOAT getAnaGain(int k) const { return AnaGain[k]; } 
+  STRING getAnaUnit(int k) const { return AnaUnit[k]; }
+  STRING getAnaName(int k) const { return AnaName[k]; }
+  LONG getIFirstImage() const { return IFirstImage; }
+  DWORD getdwImageCount() const { return dwImageCount; }
+  SHORT getnQFactor() const { return nQFactor; }
+  WORD getwCineFileType() const { return wCineFileType; }
+  STRING getszCinePath(int k) const { return szCinePath[k]; }
+  WORD  getImWidth() const { return ImWidth; }
+  WORD getImHeight() const { return ImHeight; }
+  WORD getEDRShutter16() const { return EDRShutter16; }
+  UINT getSerial() const { return Serial; }
+  INT getSaturation() const {return Saturation; }
+  UINT getAutoExposure() const { return AutoExposure; }
+  BOOL getbFlipH() const { return bFlipH; }
+  BOOL getbFlipV() const { return bFlipV; }
+  UINT getGrid() const { return Grid; }
+  UINT getFrameRate() const { return FrameRate; }
+  UINT getShutter() const { return Shutter; }
+  UINT getEDRShutter() const { return EDRShutter; }
+  UINT getPostTrigger() const { return PostTrigger; }
+  UINT getFrameDelay() const { return FrameDelay; }
+  BOOL getbEnableColor() const { return bEnableColor; }
+  UINT getCameraVersion() const { return CameraVersion; }
+  UINT getFirmwareVersion() const { return FirmwareVersion; }
+  UINT getSoftwareVersion() const { return SoftwareVersion; }
+  INT getRecordingTimeZone() const { return RecordingTimeZone; }
+  UINT getCFA() const { return CFA; }
+  INT getBright() const { return Bright; }
+  INT getContrast() const { return Contrast; }
+  INT getGamma() const { return Gamma; }
+  UINT getAutoExpLevel() const { return AutoExpLevel; }
+  UINT getAutoExpSpeed() const { return AutoExpSpeed; }
+  WORD getAutoExpRect(int k) const { return AutoExpRect[k]; }
+  WBGAIN getWBGain(int k) const { return WBGain[k]; }
+  INT getRotate() const { return Rotate; }
+  WBGAIN getWBView() const { return WBView; }
+  UINT getRealBPP() const { return RealBPP; }
+  UINT getConv8Min() const { return Conv8Min; }
+  UINT getConv8Max() const { return Conv8Max; }
+  INT getFilterCode() const { return FilterCode; }
+  INT getFilterParam() const { return FilterParam; }
+  IMFILTER getUF() const { return UF; }
+  UINT getBlackCalSVer() const { return BlackCalSVer; }
+  UINT getWhiteCalSVer() const { return WhiteCalSVer; }
+  UINT getGrayCalSVer() const { return GrayCalSVer; }
+  BOOL getbStampTime() const { return bStampTime; }
+  UINT getSoundDest() const { return SoundDest; }
+  UINT getFRPSteps() const { return FRPSteps; }
+  INT getFRPImgNr( int k) const { return FRPImgNr[k]; }
+  UINT getFRPRate(int k) const { return FRPRate[k]; }
+  UINT getFRPExp(int k) const { return FRPExp[k]; }
+  INT getMCCnt() const { return MCCnt; }
+  FLOAT getMCPercent(int k) const { return MCPercent[k]; }
+  UINT getCICalib() const { return CICalib; }
+  UINT getCalibWidth() const { return CalibWidth; }
+  UINT getCalibHeight() const { return CalibHeight; }
+  UINT getCalibRate() const { return CalibRate; }
+  UINT getCalibExp() const { return CalibExp; }
+  UINT getCalibEDR() const { return CalibEDR; }
+  UINT getCalibTemp() const { return CalibTemp; }
+  UINT getHeadSerial(int k) const { return HeadSerial[k]; }
+  UINT getRangeCode() const { return RangeCode; }
+  UINT getRangeSize() const { return RangeSize; }
+  UINT getDecimation() const { return Decimation; }
+  UINT getMasterSerial() const { return MasterSerial; }
+  UINT getSensor() const { return Sensor; }
+  UINT getShutterNs() const { return ShutterNs; }
+  UINT getEDRShutterNs() const { return EDRShutterNs; }
+  UINT getFrameDelayNs() const { return FrameDelayNs; }
+  UINT getImPosXAcq() const { return ImPosXAcq; }
+  UINT getImPosYAcq() const { return ImPosYAcq; }
+  UINT getImWidthAcq() const { return ImWidthAcq; }
+  UINT getImHeightAcq() const { return ImHeightAcq; }
+  STRING getDescription() const { return Description; }
+
+
  private:
   // Obsolete variables that are no longer used
   /*
@@ -29,7 +132,8 @@ class SETUP {
   BYTE ShutterOn; //obsolete
   CHAR * DescriptionOLD; //obsolete
   */
-  WORD Mark;
+
+
   WORD Length;
   WORD Binning; //obsolete
   WORD SigOption;
@@ -48,7 +152,7 @@ class SETUP {
   DWORD dwImageCount;
   SHORT nQFactor;
   WORD wCineFileType;
-  STRING szCinePath;
+  STRING szCinePath[4];
   WORD bMainsFreq; //obsolete
   BYTE bTimeCode; //obsolete
   BYTE bPriority; //obsolete
@@ -102,7 +206,7 @@ class SETUP {
   BOOL bStampTime;
   UINT SoundDest;
   UINT FRPSteps;
-  INT FRPImgNR[16];
+  INT FRPImgNr[16];
   UINT FRPRate[16];
   UINT FRPExp[16];
   INT MCCnt;
@@ -143,6 +247,7 @@ class SETUP {
 std::ostream& operator<<(std::ostream& os, const SETUP & setupheader);
 
 class exceptionMarkNotFound : public std::exception {
+
   virtual const char * what() const throw() 
   {
     return "Mark not found in Cine file";
